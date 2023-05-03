931e7d217009268c7c9e282e27897b6616aa9e90
Aung Kyaw Phyo (Sebastian Kein) <94116245+AungKyawPhyo1142@users.noreply.github.com>
2023-05-03 08:29:42 +0630

    create TextInput Component Done with Register & Login View

---
+++ src/views/LoginView.vue
@@ -0,0 +1,77 @@
+<template>
+    <div id="Login">
+        <div class="w-full p-6 flex justify-center items-center">
+            <div class="w-full max-w-xs">
+
+            <div class="bg-black p-8 shadow rounded mb-6">
+                <h1 class="mb-6 text-lg text-gray-100 font-thin">
+                    Let's get Rocking
+                </h1>
+
+                <div class="mb-4">
+
+                    <div class="mb-4">
+                    <TextInput
+                    label="Email"
+                    :labelColor=false
+                    placeholder="john.doe@gmail.com"
+                    v-model:input="email"
+                    inputType="text"
+                    error="This is a test error"
+                    />
+                </div>
+                <div class="mb-4">
+                    <TextInput
+                    label="Password"
+                    :labelColor=false
+                    placeholder="password123"
+                    v-model:input="password"
+                    inputType="password"
+                    error="This is a test error"
+                    />
+                    
+                </div>
+
+
+                </div>
+
+                <button 
+                    class="
+                    block
+                    w-full
+                    bg-green-500
+                    text-white
+                    rounded-sm
+                    py-3
+                    text-sm
+                    tracking-wide
+                    "
+                    type="submit"
+                    >
+                Register
+                </button>
+            </div>
+            <p class="text-center text-md text-gray-500">
+                Don't have an account yet?
+                <router-link to="register" class="text-blue-500 no-underline hover:underlink">
+                    Register
+                </router-link>
+            </p>
+        </div>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import TextInput from "../components/global/TextInput.vue"
+
+let email = ref(null)
+let password = ref(null)
+</script>
+
+
+<style lang="scss">
+
+</style>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,105 @@
+<template>
+    <div id="Register">
+        <div class="w-full p-6 flex justify-center items-center">
+            <div class="w-full max-w-xs">
+
+            <div class="bg-black p-8 shadow rounded mb-6">
+                <h1 class="mb-6 text-lg text-gray-100 font-thin">
+                    Let's get Rocking
+                </h1>
+                <div class="mb-4">
+                    <TextInput
+                    label="First Name"
+                    :labelColor=false
+                    placeholder="John"
+                    v-model:input="firstName"
+                    inputType="text"
+                    error="This is"
+                    />
+                    
+                </div>
+                <div class="mb-4">
+                    <TextInput
+                    label="Last Name"
+                    :labelColor=false
+                    placeholder="Doe"
+                    v-model:input="lastName"
+                    inputType="text"
+                    error="This is a test error"
+                    />
+                    
+                </div>
+                <div class="mb-4">
+                    <TextInput
+                    label="Email"
+                    :labelColor=false
+                    placeholder="john.doe@gmail.com"
+                    v-model:input="email"
+                    inputType="text"
+                    error="This is a test error"
+                    />
+                </div>
+                <div class="mb-4">
+                    <TextInput
+                    label="Password"
+                    :labelColor=false
+                    placeholder="password123"
+                    v-model:input="password"
+                    inputType="password"
+                    error="This is a test error"
+                    />
+                    
+                </div>
+                <div class="mb-4">
+                    <TextInput
+                    label="Confirm Password"
+                    :labelColor=false
+                    placeholder="password123"
+                    v-model:input="confirmPassword"
+                    inputType="password"
+                    error="This is a test error"
+                    />
+
+                </div>
+                <button 
+                    class="
+                    block
+                    w-full
+                    bg-green-500
+                    text-white
+                    rounded-sm
+                    py-3
+                    text-sm
+                    tracking-wide
+                    "
+                    type="submit"
+                    >
+                Register
+                </button>
+            </div>
+            <p class="text-center text-md text-gray-500">
+                Already have an account?
+                <router-link to="login" class="text-blue-500 no-underline hover:underlink">
+                    Login
+                </router-link>
+            </p>
+        </div>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import TextInput from "../components/global/TextInput.vue"
+
+let firstName = ref(null)
+let lastName = ref(null)
+let email = ref(null)
+let password = ref(null)
+let confirmPassword = ref(null)
+</script>
+
+<style lang="scss">
+
+</style>
